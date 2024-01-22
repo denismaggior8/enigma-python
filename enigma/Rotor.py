c52b4ed67bf6bb117a5ff0f4f38b96af2dd00aee
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-01-22 11:02:23 +0100

    Rotor tests

--- enigma/Rotor.py
+++ enigma/Rotor.py
@@ -19,8 +19,6 @@ class Rotor:
         return dictionary[scrambled_letter_index_from_rotor]
 
     def __init__(self, wiring, position):
-        if wiring == None:
-            wiring = list(ascii_lowercase)
         self.wiring = wiring
         self.position = position % len(wiring)
 
@@ -31,21 +29,27 @@ class Rotor:
 class TestRotor(unittest.TestCase):
     
     def test_rotor_position_increment_by_1(self):
-        rotor = Rotor(None,0)
+        rotor = Rotor(list(ascii_lowercase),0)
         rotor.increment_position()
         self.assertEqual(rotor.position, 1, "Rotor position is wrong")
+
+    def test_rotor_position_increment_by_1_over_boundary(self):
+        rotor = Rotor(list(ascii_lowercase),25)
+        rotor.increment_position()
+        self.assertEqual(rotor.position, 0, "Rotor position is wrong")
     
     def test_rotor_multiple_position_increment(self):
-        rotor = Rotor(None,0)
+        rotor = Rotor(list(ascii_lowercase),0)
         random_int = random.randint(0, 10000)
         for i in range(random_int):
             rotor.increment_position()
         self.assertEqual(rotor.position, random_int % len(rotor.wiring), "Rotor position is wrong")
     
     def test_set_rotor_position(self):
-        rotor = Rotor(None,0)
+        rotor = Rotor(list(ascii_lowercase),0)
         random_int = random.randint(0, 10000)
         rotor.set_position(random_int)
         self.assertEqual(rotor.position, random_int % len(rotor.wiring), "Rotor position is wrong")
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
