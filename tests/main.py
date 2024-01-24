6f7ab164f5dc0a8f11a5d8b5385fd9d443c0a61c
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-01-24 10:48:07 +0100

    fix tests main

--- tests/main.py
+++ tests/main.py
@@ -5,11 +5,9 @@ from PlugboardPassthrough import PlugboardPassthrough
 from ReflectorUKWB import ReflectorUKWB
 from EnigmaM3 import EnigmaM3
 from EtwPassthrough import EtwPassthrough
-
 import unittest
 
-suite = unittest.TestLoader().discover(start_dir="tests",pattern="*_test*.py")
-unittest.TextTestRunner(verbosity=2).run(suite)
 
-if __name__ == "__main__":
-    unittest.main()
+if __name__ == "__main__":   
+    suite = unittest.TestLoader().discover(start_dir="tests",pattern="*_test*.py")
+    unittest.TextTestRunner(verbosity=2).run(suite)
