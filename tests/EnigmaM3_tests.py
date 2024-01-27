b961f97f01e1a60cbddd608d572cdfeb70ee1544
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-01-27 17:27:27 +0100

    More long string tests

--- tests/EnigmaM3_tests.py
+++ tests/EnigmaM3_tests.py
@@ -330,6 +330,44 @@ class TestEnigmaM3(unittest.TestCase):
         other_machine.set_display('AAA')
         other_encrypted_string = other_machine.process_text(cleartext)
         self.assertEqual(my_encrypted_string,other_encrypted_string.lower(),"Enigma encryption error")
+
+    def test_enigma_3_rotors_III_IV_V_rotations_from_0_very_long_string(self):
+        plugboard = PlugboardPassthrough()
+        rotor1 = EnigmaM3RotorV(0)
+        rotor2 = EnigmaM3RotorIV(0)
+        rotor3 = EnigmaM3RotorIII(0)
+        reflector = ReflectorUKWB()
+        etw = EtwPassthrough()
+        enigma = EnigmaM3(plugboard, rotor3, rotor2, rotor1, reflector, etw, True)
+        cleartext = 'd' * 100000
+        my_encrypted_string = enigma.input_string(cleartext)
+        other_machine = EnigmaMachine.from_key_sheet(
+            rotors='V IV III',
+            reflector='B',
+            ring_settings=[0, 0, 0],
+            plugboard_settings=None)
+        other_machine.set_display('AAA')
+        other_encrypted_string = other_machine.process_text(cleartext)
+        self.assertEqual(my_encrypted_string,other_encrypted_string.lower(),"Enigma encryption error")
+
+    def test_enigma_3_rotors_IV_V_VI_rotations_from_0_very_long_string(self):
+        plugboard = PlugboardPassthrough()
+        rotor1 = EnigmaM3RotorVI(0)
+        rotor2 = EnigmaM3RotorV(0)
+        rotor3 = EnigmaM3RotorIV(0)
+        reflector = ReflectorUKWB()
+        etw = EtwPassthrough()
+        enigma = EnigmaM3(plugboard, rotor3, rotor2, rotor1, reflector, etw, True)
+        cleartext = 'd' * 100000
+        my_encrypted_string = enigma.input_string(cleartext)
+        other_machine = EnigmaMachine.from_key_sheet(
+            rotors='VI V IV',
+            reflector='B',
+            ring_settings=[0, 0, 0],
+            plugboard_settings=None)
+        other_machine.set_display('AAA')
+        other_encrypted_string = other_machine.process_text(cleartext)
+        self.assertEqual(my_encrypted_string,other_encrypted_string.lower(),"Enigma encryption error")
         
 
 if __name__ == "__main__":
