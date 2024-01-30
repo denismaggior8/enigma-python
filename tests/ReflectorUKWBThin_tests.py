7aa773d0a4e4c1754945bf2b361b2b60a9b1d232
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-01-30 13:28:51 +0100

    ReflectorUKWBThin_tests

---
+++ tests/ReflectorUKWBThin_tests.py
@@ -0,0 +1,15 @@
+
+from ReflectorUKWBThin import ReflectorUKWBThin
+from string import ascii_lowercase
+import unittest
+
+class TestReflectorUKWB(unittest.TestCase):
+    
+    def test_reflector_b_scramble_letter_index_z(self):
+        reflector = ReflectorUKWBThin()
+        char = "z"
+        scrambled_char = reflector.scramble_letter_index(reflector.wiring,list(ascii_lowercase).index(char))
+        self.assertEqual(scrambled_char,"s","Scramble error")
+
+if __name__ == "__main__":
+    unittest.main()
