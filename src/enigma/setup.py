e9fc0aeb0473e82d0f247776ff48033284bc4be1
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-05-02 09:32:14 +0200

    setup.py

--- src/enigma/setup.py
+++ src/enigma/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="enigmapython",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(
         # All keyword arguments below are optional:
         where='.',  # '.' by default
