b95322e51935ab891f39dc285cb61cb851a727b7
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-05-02 10:28:07 +0200

    fix setup.py

--- src/enigma/setup.py
+++ src/enigma/setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 
 setup(
     author="Denis Maggiorotto",
-    long_description=open('enigmapython/README.txt').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/denismaggior8/enigma-python",
     name="enigmapython",
-    version="v0.1.1",
+    version="v0.1.2",
     packages=find_packages(
         # All keyword arguments below are optional:
         where='.',  # '.' by default
