cbe036821d30a0d95f4a616c1ea5db1571d23929
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-05-02 10:31:54 +0200

    fix setup.py

--- src/enigma/setup.py
+++ src/enigma/setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory/"enigmapython"/"README.md").read_text()
 
 
 setup(
