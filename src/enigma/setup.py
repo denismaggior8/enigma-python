85c0ee74da22de7ef39ef1e39391ec1f60b413cf
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-05-02 10:22:25 +0200

    fix setup.py

--- src/enigma/setup.py
+++ src/enigma/setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 
 
 setup(
+    author="Denis Maggiorotto",
+    long_description=open('enigmapython/README.txt').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/denismaggior8/enigma-python",
     name="enigmapython",
-    version="0.1.0",
+    version="v0.1.1",
     packages=find_packages(
         # All keyword arguments below are optional:
         where='.',  # '.' by default
         include=['enigmapython'],  # ['*'] by default
     ),
-    long_description=open('./enigmapython/README.txt').read(),
-    long_description_content_type='text/markdown'
+    description="A simple yet faithful library to emulate different Enigma machines models using Python"
 )
