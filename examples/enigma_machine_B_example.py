f7bb7bcc300fe6ba85f3c4e8ebbb125038223606
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-12-26 17:53:43 +0100

    More logs and .gitignore

--- examples/enigma_machine_B_example.py
+++ examples/enigma_machine_B_example.py
@@ -1,7 +1,6 @@
 from enigmapython.EnigmaB_A133RotorI import EnigmaB_A133RotorI
 from enigmapython.EnigmaB_A133RotorII import EnigmaB_A133RotorII
 from enigmapython.EnigmaB_A133RotorIII import EnigmaB_A133RotorIII
-from enigmapython.PlugboardPassthrough import PlugboardPassthrough
 from enigmapython.EnigmaB_A133Etw import EnigmaB_A133Etw
 from enigmapython.EnigmaB_A133 import EnigmaB_A133
 from enigmapython.ReflectorUKW_EnigmaB_A133 import ReflectorUKW_EnigmaB_A133
@@ -16,8 +15,8 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 # Setup Enigma B components (compared to other machine types, Enigma B doesn't have a plugboard)
 rotor1 = EnigmaB_A133RotorI(0)
-rotor2 = EnigmaB_A133RotorII(0)
-rotor3 = EnigmaB_A133RotorIII(5)
+rotor2 = EnigmaB_A133RotorI(0)
+rotor3 = EnigmaB_A133RotorI(0)
 reflector = ReflectorUKW_EnigmaB_A133()
 etw = EnigmaB_A133Etw()
 
@@ -30,4 +29,7 @@ enigma = EnigmaB_A133(rotor3, rotor2, rotor1, reflector, etw, True)
 # Test the Enigma machines
 if __name__ == "__main__":
 
-   print(enigma.input_string("s"))
+   print(enigma.input_string("ettbesettbesettbesettbesettbesettbesettbesettbesettbesettbesettbesettbes"))
+   print(enigma.rotors[0])
+   print(enigma.rotors[1])
+   print(enigma.rotors[2])
