599952de2f22c3d5ff1310d88262895d5331e0a0
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2025-12-09 12:42:18 +0100

    feat: remove Enigma D ETW and its shared JWULCM keyboard layout classes

--- src/enigma/enigmapython/EnigmaDEtw_JWULCM.py
+++
@@ -1,5 +0,0 @@
-from .EtwJWULCM import EtwJWULCM
-
-class EnigmaDEtw_JWULCM(EtwJWULCM):
-    """Enigma D ETW with JWULCM keyboard layout"""
-    pass
--- src/enigma/enigmapython/EtwJWULCM.py
+++
@@ -1,7 +0,0 @@
-from .Etw import Etw
-
-class EtwJWULCM(Etw):
-    """Shared JWULCM keyboard layout ETW used by Enigma D"""
-    wiring = "jwulcmnohpqzyxiradkegvbtsf"
-    def __init__(self):
-         super().__init__(self.wiring)
